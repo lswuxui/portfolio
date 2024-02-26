290c96389d80d7426ecf29f62e19925ea0415ae8
lswuxui <137474451+lswuxui@users.noreply.github.com>
2024-02-27 04:47:55 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -39,6 +39,7 @@ function init(){
       document.getElementsByClassName('chuvaMeteoro')[0].innerHTML = "";
     }, 1000);
   }
+  
 }
 window.onload = init;
 // -------------------------
@@ -137,5 +138,18 @@ const repeat = setInterval(() => {
   barAnimation() || barAnimation2() || barAnimation3()
   donutAnimation() || donutAnimation2()
 })
+var slideIndex = 0;
+carousel();
 
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("myslide");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 300);
+}
 // ------------------------------------------------------
